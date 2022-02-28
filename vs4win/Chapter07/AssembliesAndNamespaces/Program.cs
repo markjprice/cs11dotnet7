c0d4d11b545ebe6965e740369ebf0202e8f04d09
Mark J Price <markjprice@gmail.com>
2022-02-28 20:09:20 +0000

    Initial commit

---
+++ vs4win/Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Xml.Linq; // XDocument
+using System; // String
+using Packt.Shared; // IsValidHex(), IsValidXmlTag(), IsValidPassword()
+using DialectSoftware.Collections;
+using DialectSoftware.Collections.Generics;
+
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
+
+Write("Enter a color value in hex: ");
+string? hex = ReadLine(); // or "00ffc8"
+WriteLine("Is {0} a valid color value? {1}",
+  arg0: hex, arg1: hex.IsValidHex());
+
+Write("Enter a XML element: ");
+string? xmlTag = ReadLine(); // or "<h1 class=\"<\" />"
+WriteLine("Is {0} a valid XML element? {1}",
+  arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+
+Write("Enter a password: ");
+string? password = ReadLine(); // or "secretsauce"
+WriteLine("Is {0} a valid password? {1}",
+  arg0: password, arg1: password.IsValidPassword());
+
+Axis x = new("x", 0, 10, 1);
+Axis y = new("y", 0, 4, 1);
+Matrix<long> matrix = new(new[] { x, y });
+
+for (int i = 0; i < matrix.Axes[0].Points.Length; i++)
+{
+  matrix.Axes[0].Points[i].Label = "x" + i.ToString();
+}
+
+for (int i = 0; i < matrix.Axes[1].Points.Length; i++)
+{
+  matrix.Axes[1].Points[i].Label = "y" + i.ToString();
+}
+
+foreach (long[] c in matrix)
+{
+  matrix[c] = c[0] + c[1];
+}
+
+foreach (long[] c in matrix)
+{
+  WriteLine("{0},{1} ({2},{3}) = {4}",
+    matrix.Axes[0].Points[c[0]].Label,
+    matrix.Axes[1].Points[c[1]].Label,
+    c[0], c[1], matrix[c]);
+}
---
+++ vs4win/Chapter07/DotNetEverywhere/build-all-platforms.ps1
@@ -0,0 +1,5 @@
+dotnet publish -c Release -r win10-x64
+dotnet publish -c Release -r osx-x64
+dotnet publish -c Release -r osx.11.0-arm64
+dotnet publish -c Release -r linux-x64
+dotnet publish -c Release -r linux-arm64
