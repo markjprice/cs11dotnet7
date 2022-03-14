e13b6e59f1a0bebce61fa0c328854baa815f415d
Mark J Price <markjprice@gmail.com>
2022-03-14 08:44:08 +0000

    Initial commit

---
+++ vscode/Chapter09/WorkingWithRandomAccess/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32.SafeHandles; // SafeFileHandle
+using System.Text; // Encoding
+
+using SafeFileHandle handle =
+  File.OpenHandle(path: "coffee.txt", 
+    mode: FileMode.OpenOrCreate,
+    access: FileAccess.ReadWrite);
+
+// write to the file
+string message = "Café £4.39";
+ReadOnlyMemory<byte> buffer = new(Encoding.UTF8.GetBytes(message));
+await RandomAccess.WriteAsync(handle, buffer, fileOffset: 0);
+
+// read from the file
+long length = RandomAccess.GetLength(handle);
+Memory<byte> contentBytes = new(new byte[length]);
+await RandomAccess.ReadAsync(handle, contentBytes, fileOffset: 0);
+string content = Encoding.UTF8.GetString(contentBytes.ToArray());
+WriteLine($"Content of file: {content}");
+
