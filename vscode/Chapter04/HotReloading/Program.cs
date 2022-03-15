f7cc057037a890da964b4cf6a89d73386f156178
Mark J Price <markjprice@gmail.com>
2022-03-15 08:17:56 +0000

    Initial commit

---
+++ vscode/Chapter04/HotReloading/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Visual Studio: run the app, change the message, click Hot Reload button.
+ * Visual Studio Code: run the app using dotnet watch, change the message. */
+
+while (true)
+{
+  WriteLine("Goodbye, Hot Reload!");
+  await Task.Delay(2000);
+}
