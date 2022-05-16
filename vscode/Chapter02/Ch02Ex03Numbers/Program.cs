8b69345464545d0d1dd4e525c6df366fd182f8ce
Mark J Price <markjprice@gmail.com>
2022-05-16 11:44:00 +0100

    Initial commit

--- vscode/Chapter02/Ch02Ex03Numbers/Program.cs
+++ vscode/Chapter02/Ch02Ex03Numbers/Program.cs
@@ -1,7 +1,7 @@
-﻿string tableRowSeparator = new string('-', 74);
-WriteLine(tableRowSeparator);
+﻿string rowSeparator = new string('-', count: 74);
+WriteLine(rowSeparator);
 WriteLine("Type    Byte(s) of memory               Min                            Max");
-WriteLine(tableRowSeparator);
+WriteLine(rowSeparator);
 WriteLine($"sbyte   {sizeof(sbyte),-4} {sbyte.MinValue,30} {sbyte.MaxValue,30}");
 WriteLine($"byte    {sizeof(byte),-4} {byte.MinValue,30} {byte.MaxValue,30}");
 WriteLine($"short   {sizeof(short),-4} {short.MinValue,30} {short.MaxValue,30}");
@@ -13,4 +13,4 @@ WriteLine($"ulong   {sizeof(ulong),-4} {ulong.MinValue,30} {ulong.MaxValue,30}")
 WriteLine($"float   {sizeof(float),-4} {float.MinValue,30} {float.MaxValue,30}");
 WriteLine($"double  {sizeof(double),-4} {double.MinValue,30} {double.MaxValue,30}");
 WriteLine($"decimal {sizeof(decimal),-4} {decimal.MinValue,30} {decimal.MaxValue,30}");
-WriteLine(tableRowSeparator);
+WriteLine(rowSeparator);
